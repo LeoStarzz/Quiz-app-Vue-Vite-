8c29e71f6c6fa08c5f35ff9bf129ddefd1319820
LeoStarzz <neytron888@gmail.com>
2022-07-27 14:11:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-htnb2v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-htnb2v)
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+const questions = [
+  {
+    question: '',
+  },
+];
+</script>
+
+<template>
+  <div>Hello world</div>
+</template>
+
+<style scoped>
+div {
+  width: 100px;
+  height: 100px;
+  background-color: black;
+}
+</style>
